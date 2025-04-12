5ee1a59cd0e7c3d4b187994b31b614429adaba27
Uchôa <uchoadev16@gmail.com>
2025-04-12 10:26:36 -0300

    Revert "test"
    This reverts commit 805ee97267610d461e2c91e5311609d0c31689e3.

--- app/main/views/QRCode/tes.php
+++
@@ -1,8 +0,0 @@
-<?php
-$aux = './qrcode/php/qr_img.php?';
-$aux .= 'd=https://salaberga.com&e=M&s=5&s=4';
-echo $aux;
-?>
-
-<img src="./qr_img0.50/php/qr_img.php?d=https%3A%2F%2Fsalaberga.com%2Fsalaberga%2Fportalsalaberga%2Fapp%2Fmain%2Fviews%2Fsubsytem%2Fsubsistema.php%3F123&amp;e=Q&amp;s=5&amp;v=15">
-
